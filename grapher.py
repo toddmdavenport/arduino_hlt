231a6f2a665f986560a7407a3e21e03ab567289d
Todd Davenport <tdavenport@quisling.(none)>
2012-12-03 21:05:53 -0500

    parsing data output

---
+++ grapher.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import csv, matplotlib
+
+def csv_parser(date):
+    with open('data/' + date, 'rb') as csvfile:
+        data = csv.reader(csvfile, delimiter=",")
+        time, set_t, probe = [], [], []
+        for row in data:
+            time.append(row[0]) 
+            set_t.append(row[1])
+            probe.append(row[2])
+    print time 
+    print set_t
+    print probe
+
+csv_parser('2012-11-30')
+
+
